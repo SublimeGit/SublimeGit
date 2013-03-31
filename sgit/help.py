d3e525c41db1f3d05d5086463d94d00511182f6d
Michael Pedersen <mp@beefy.dk>
2013-03-31 13:06:28 +0200

    Implemented fancy help

--- sgit/help.py
+++ sgit/help.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+import re
 import webbrowser
 try:
     from xml.etree import cElementTree as ET
@@ -12,7 +13,78 @@ from sublime_plugin import WindowCommand
 from .cmd import GitCmd
 
 
+MANPAGE_RE = re.compile(r'\(\d\) Manual Page\s*')
+
+
 class GitHelpCommand(WindowCommand, GitCmd):
+
+    def get_doc_files(self, doc_path):
+        files = []
+        for f in os.listdir(doc_path):
+            if not f.endswith('.html'):
+                continue
+            if f == 'index.html':
+                continue
+            files.append(os.path.join(doc_path, f))
+        return files
+
+    def format_choices(self, doc_files, fancy=False):
+        formatter = self.format_fancy if fancy else self.format_plain
+
+        choices = []
+        for f in doc_files:
+            url = 'file://%s' % f
+            choice = formatter(f)
+            if choice:
+                choices.append((choice, url))
+
+        return sorted(choices)
+
+    def format_fancy(self, filename):
+        # deal wth a couple of special files
+        if filename.endswith('everyday.html'):
+            return [u'Everyday GIT With 20 Commands Or So',
+                    u'Individual Developer (Standalone) commands are essential for anybody ' +
+                    'who makes a commit, even for somebody who works alone.']
+        elif filename.endswith('user-manual.html'):
+            return [u"Git User's Manual",
+                    u"Git is a fast distributed revision control system."]
+
+        title = os.path.basename(filename)[:-5]
+        text = '[no summary]'
+
+        try:
+            root = ET.parse(filename).getroot()
+        except Exception:
+            return [title, '[could not parse file]']
+
+        h1 = root.find(".//{http://www.w3.org/1999/xhtml}h1")
+        p = root.find(".//{http://www.w3.org/1999/xhtml}p")
+
+        if h1 is not None and p is not None:
+            if h1.text and p.text:
+                title = h1.text
+                content = p.text
+
+                if MANPAGE_RE.search(title):
+                    # We are dealing with a manpage
+                    title = MANPAGE_RE.sub('', title)
+                    _, secondline = content.split('\n', 1)
+                    text = secondline.strip().replace('\n', ' ')
+                else:
+                    text = content.replace('\n', ' ').strip()
+
+        if len(text) > 100:
+            text = text[:100]
+
+        return [title, text]
+
+    def format_plain(self, filename):
+        basename = os.path.basename(filename)
+        noext = basename[:-5]
+        text = noext[4:] if noext.startswith('git-') else noext
+
+        return text
 
     def run(self):
         doc_path = self.git_string(['--html-path'], cwd=os.path.realpath(''))
@@ -20,56 +92,15 @@ class GitHelpCommand(WindowCommand, GitCmd):
             sublime.error_message('Directory %s does not exist. Have you deleted the git documentation?' % doc_path)
             return
 
-        doc_files = {}
-        for f in os.listdir(doc_path):
-            if not f.endswith('.html'):
-                continue
-            key = f[4:-5] if f.startswith('git-') else f[:-5]
-            url = 'file://%s' % os.path.join(doc_path, f)
-            doc_files[key] = url
-
-        choices = sorted(doc_files.keys())
+        doc_files = self.get_doc_files(doc_path)
+        choices = self.format_choices(doc_files, fancy=True)
 
         def on_done(idx):
             if idx != -1:
-                choice = choices[idx]
-                webbrowser.open(doc_files[choice])
+                text, url = choices[idx]
+                webbrowser.open(url)
 
-        self.window.show_quick_panel(choices, on_done)
-
-
-class GitHelpExtendedCommand(WindowCommand, GitCmd):
-
-    def run(self):
-        doc_path = self.git_string(['--html-path'], cwd=os.path.realpath(''))
-
-        if not os.path.exists(doc_path):
-            sublime.error_message('Directory %s does not exist. Have you deleted the git documentation?' % doc_path)
-            return
-
-        choices = []
-        for f in os.listdir(doc_path):
-            if not f.endswith('.html'):
-                continue
-
-            try:
-                print os.path.join(doc_path, f)
-                tree = ET.parse(os.path.join(doc_path, f))
-                root = tree.getroot()
-                h1 = root.find(".//{http://www.w3.org/1999/xhtml}h1").text.replace('Manual Page', '')
-                firstp = root.find(".//{http://www.w3.org/1999/xhtml}p").text.split('\n', 1)[1].strip()
-                choices.append([h1.replace('(1)', ''), firstp])
-            except Exception, e:
-                print e
-                # handle user manual?
-                pass
-
-        print choices
-
-        def on_done(idx):
-            pass
-
-        self.window.show_quick_panel(choices, on_done)
+        self.window.show_quick_panel([t for t, u in choices], on_done)
 
 
 class GitVersionCommand(WindowCommand, GitCmd):
