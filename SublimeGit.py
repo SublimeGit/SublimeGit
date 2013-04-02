f23ea864b675324f5a0aebe0bcf640c0034d5a81
Michael Pedersen <mp@beefy.dk>
2013-04-02 22:22:36 +0200

    Shit son... added fabfile stuff to cut and push a commit to github

--- SublimeGit.py
+++ SublimeGit.py
@@ -8,8 +8,7 @@ import sublime
 settings = sublime.load_settings('SublimeGit.sublime-settings')
 
 loglevel = getattr(logging, settings.get('log_level', '').upper(), logging.WARNING)
-logging.basicConfig(level=loglevel,
-    format="[%(asctime)s - %(levelname)s - %(name)s] %(message)s")
+logging.basicConfig(level=loglevel, format="[%(asctime)s - %(levelname)s - %(name)s] %(message)s")
 
 logger = logging.getLogger('sgit')
 
@@ -42,7 +41,7 @@ LOAD_ORDER = [
     'sgit.git_flow'
 ]
 
-needs_reload = [n for n, m in sys.modules.items() if n[0:4] == 'sgit' and m != None]
+needs_reload = [n for n, m in sys.modules.items() if n[0:4] == 'sgit' and m is not None]
 
 reloaded = []
 for module in LOAD_ORDER:
@@ -54,7 +53,6 @@ if reloaded:
 
 
 # import commands and listeners
-from sgit import DebugReloader
 
 from sgit.util import GitPanelWriteCommand, GitPanelAppendCommand
 
@@ -71,35 +69,36 @@ from sgit.help import GitHelpCommand, GitVersionCommand
 from sgit.log import GitLogCommand, GitQuickLogCommand, GitQuickLogCurrentFileCommand
 
 from sgit.remote import (GitPushCurrentBranchCommand, GitPullCurrentBranchCommand,
-    GitPushPullAllCommand, GitFetchCommand, GitPullCommand, GitPushCommand,
-    GitRemoteCommand, GitRemoteAddCommand)
+                         GitPushPullAllCommand, GitFetchCommand, GitPullCommand, GitPushCommand,
+                         GitRemoteCommand, GitRemoteAddCommand)
 
 from sgit.status import (GitStatusCommand, GitStatusRefreshCommand, GitQuickStatusCommand,
-    GitStatusMoveCommand, GitStatusStageCommand,
-    GitStatusUnstageCommand, GitStatusDiscardCommand,
-    GitStatusOpenFileCommand, GitStatusDiffCommand, GitStatusIgnoreCommand,
-    GitStatusStashCmd, GitStatusStashApplyCommand, GitStatusStashPopCommand)
+                         GitStatusMoveCommand, GitStatusStageCommand,
+                         GitStatusUnstageCommand, GitStatusDiscardCommand,
+                         GitStatusOpenFileCommand, GitStatusDiffCommand,
+                         GitStatusIgnoreCommand, GitStatusStashCmd, GitStatusStashApplyCommand,
+                         GitStatusStashPopCommand)
 from sgit.status import GitStatusBarEventListener, GitStatusEventListener
 
 from sgit.add import GitQuickAddCommand, GitAddCurrentFileCommand
 
 from sgit.commit import (GitCommitCommand, GitCommitTemplateCommand,
-    GitCommitPerformCommand, GitQuickCommitCommand)
+                         GitCommitPerformCommand, GitQuickCommitCommand)
 from sgit.commit import GitCommitEventListener
 
 from sgit.stash import (GitStashCommand, GitSnapshotCommand,
-    GitStashApplyCommand, GitStashPopCommand)
+                        GitStashApplyCommand, GitStashPopCommand)
 
 from sgit.checkout import (GitCheckoutBranchCommand, GitCheckoutCommitCommand,
-    GitCheckoutNewBranchCommand)
+                           GitCheckoutNewBranchCommand)
 
 from sgit.merge import GitMergeCommand
 
 # import legit if enabled
 if settings.get('git_extensions', {}).get('legit', True):
     from sgit.legit import (LegitSwitchCommand, LegitSyncCommand, LegitPublishCommand,
-        LegitUnpublishCommand, LegitHarvestCommand, LegitSproutCommand, LegitGraftCommand,
-        LegitBranchesCommand)
+                            LegitUnpublishCommand, LegitHarvestCommand, LegitSproutCommand,
+                            LegitGraftCommand, LegitBranchesCommand)
 
 
 # shut down logging
--- sgit/__init__.py
+++ sgit/__init__.py
@@ -1,18 +1,3 @@
 # coding: utf-8
-import os
 
-import sublime
-from sublime_plugin import EventListener
-
-
-SUBLIME_GIT = os.path.join(sublime.packages_path(), 'SublimeGit/SublimeGit.py')
-
-
-class DebugReloader(EventListener):
-
-    def on_post_save(self, view):
-        if "sgit/" in view.file_name():
-            window = view.window()
-            window.open_file(SUBLIME_GIT)
-            window.run_command('save')
-            window.focus_view(view)
+__version__ = '0.1.0'
